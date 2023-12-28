df2b68cf871ccc6638a362fb62a30a9895fd7cf2
legend5661 <wangtiantong1@126.com>
2023-12-28 14:11:44 +0800

    add some model:   nnunetv2/mymodel/mymodel.py

--- nnunetv2/mymodel/mymodel.py
+++ nnunetv2/mymodel/mymodel.py
@@ -23,4 +23,36 @@ def get_my_network_from_plans(plans_manager: PlansManager,
         model = smp.MAnet(encoder_name='resnet34', encoder_depth=5, encoder_weights='imagenet', 
                           decoder_use_batchnorm=True, decoder_channels=(256, 128, 64, 32, 16), 
                           decoder_pab_channels=64, in_channels=num_input_channels, classes=label_manager.num_segmentation_heads)
+
+    elif(model == 'linknet'):
+        model = smp.Linknet(encoder_name='resnet34', encoder_depth=5, encoder_weights='imagenet', 
+                            decoder_use_batchnorm=True, in_channels=num_input_channels, classes=label_manager.num_segmentation_heads, 
+                            )
+
+    elif(model == 'fpn'):
+        model = smp.FPN(encoder_name='resnet34', encoder_depth=5, encoder_weights='imagenet', 
+                        decoder_pyramid_channels=256, decoder_segmentation_channels=128, decoder_merge_policy='add', 
+                        decoder_dropout=0.2, in_channels=num_input_channels, 
+                        classes=label_manager.num_segmentation_heads, activation=None, upsampling=4)
+
+    elif(model == 'pspnet'):
+        model = smp.PSPNet(encoder_name='resnet34', encoder_weights='imagenet', encoder_depth=3, psp_out_channels=512,
+                            psp_use_batchnorm=True, psp_dropout=0.2, in_channels=num_input_channels, 
+                            classes=label_manager.num_segmentation_heads, activation=None, upsampling=8)
+
+    elif(model == 'panet'):
+        model = smp.PAN(encoder_name='resnet34', encoder_weights='imagenet', encoder_output_stride=16, decoder_channels=32, 
+                        in_channels=num_input_channels, classes=label_manager.num_segmentation_heads, 
+                        activation=None, upsampling=4, aux_params=None)
+
+    elif(model =='deeplabv3'):
+        model = smp.DeepLabV3(encoder_name='resnet34', encoder_depth=5, encoder_weights='imagenet', 
+                            decoder_channels=256, in_channels=num_input_channels, classes=label_manager.num_segmentation_heads, 
+                            activation=None, upsampling=8, aux_params=None)
+                            
+    elif(model == 'deeplabv3+'):
+        model = smp.DeepLabV3Plus(encoder_name='resnet34', encoder_depth=5, encoder_weights='imagenet', encoder_output_stride=16, 
+                                decoder_channels=256, decoder_atrous_rates=(12, 24, 36), 
+                                in_channels=num_input_channels, classes=label_manager.num_segmentation_heads, 
+                                activation=None, upsampling=4, aux_params=None)
     return model
