dd64a1ab90ef658e524cefba9be08e723f7b3fb4
legend5661 <wangtiantong1@126.com>
2024-03-26 18:58:58 +0800

    增加了测试中输出计算量(根据测试中本数据集的输入形状)

--- nnunetv2/training/nnUNetTrainer/nnUNetTrainer_setr.py
+++ nnunetv2/training/nnUNetTrainer/nnUNetTrainer_setr.py
@@ -85,6 +85,7 @@ class nnUNetTrainer_setr(nnUNetTrainer):
             self.num_val_iterations_per_epoch = 50
             self.num_epochs = 500
             self.current_epoch = 0
+            self.batch_size = 8
 
             if self.configuration_manager.patch_size[0] > self.configuration_manager.patch_size[1]:
                 self.configuration_manager.patch_size[1]=self.configuration_manager.patch_size[0]
